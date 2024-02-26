a4bed2bc7f4b46b4238e46c9b00e8bab1c5d2c93
Taki <saihan0176@gmail.com>
2024-02-26 15:43:46 +0600

    small fix in module docsting

--- torchmate/trainer/__init__.py
+++ torchmate/trainer/__init__.py
@@ -8,15 +8,6 @@ including the model architecture, data loaders for training, validation, and opt
 and scheduling mechanisms. It also provides hooks for incorporating custom metrics and callbacks, offering extensibility for
 tailored evaluation and logging during training.
 
-**Key Features:**
-
-- `Mixed Precision Training:` Enables faster training and potentially better generalization through automatic mixed precision (AMP).
-- `Gradient Penalty:` Helps stabilize training in generative models like GANs.
-- `Callback System:` Supports custom actions at different training stages (e.g., logging, early stopping, model saving).
-- `Customizable Training and Validation:` Allows for flexible configuration of loss functions, metrics, and datasets.
-- `Experiment Tracking:` For better experiment tracking, ditch spreadsheets and use dedicated tools like Weights & Biases
-   or TensorBoard.These platforms track training, log metrics, visualize results, and organize experiments, saving you time
-   and aiding collaboration. Torchmate's Trainer integrates them seamlessly via callbacks.
 """
 
 from torchmate.trainer.trainer import Trainer
